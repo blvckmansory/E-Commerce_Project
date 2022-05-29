899a44d486b16550bb3ac2f2fad86491fb96e7b7
blvckmansory <shamaich007@gmail.com>
2022-05-29 21:46:16 +0300

    some changes

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -26,20 +26,38 @@ class Product(models.Model):
             url = ''
         return
 
-class Order(models.Model): # it is our order-object
-    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True) # many-to-one relationship, because customer can have multiple orders
-    date_ordered = models.DateTimeField(auto_now_add=True) # change the value when the order is complete
-    complete = models.BooleanField(default=False) # status of our cart
-    transaction_id = models.CharField(max_length=100, null=True) # information for order
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return str(self.id)
+
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
 
 class OrderItem(models.Model): # many-to-one relationship, because cart can have multiple order items
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
